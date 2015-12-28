b1ed5608e95a990279240d80ffc9f2dea4e357fa
Jeremiah Kistler <jeremiahkistler@gmail.com>
2015-12-27 21:57:08 -0600

    Add network manager class.

---
+++ deepThoughts/NetworkManager.h
@@ -0,0 +1,13 @@
+//
+//  NetworkManager.h
+//  deepThoughts
+//
+//  Created by Jeremiah Kistler on 12/20/15.
+//  Copyright © 2015 Jeremiah Kistler. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NetworkManager : NSObject
+
+@end
---
+++ deepThoughts/NetworkManager.m
@@ -0,0 +1,17 @@
+//
+//  NetworkManager.m
+//  deepThoughts
+//
+//  Created by Jeremiah Kistler on 12/20/15.
+//  Copyright © 2015 Jeremiah Kistler. All rights reserved.
+//
+
+#import "NetworkManager.h"
+
+@interface NetworkManager()
+
+@end
+
+@implementation NetworkManager
+
+@end
