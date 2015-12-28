d1baa02ca04eba2857e369be0ccf51e322b4eec1
Jeremiah Kistler <jeremiahkistler@gmail.com>
2015-12-27 22:14:52 -0600

    Add settingsViewController.

---
+++ deepThoughts/ViewControllers/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  deepThoughts
+//
+//  Created by Jeremiah Kistler on 12/27/15.
+//  Copyright © 2015 Jeremiah Kistler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
---
+++ deepThoughts/ViewControllers/SettingsViewController.m
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.m
+//  deepThoughts
+//
+//  Created by Jeremiah Kistler on 12/27/15.
+//  Copyright © 2015 Jeremiah Kistler. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+
+@implementation SettingsViewController
+
+@end
