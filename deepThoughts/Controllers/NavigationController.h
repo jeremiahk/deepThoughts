d24dac4c5f41b8edb60b51443f06bb6b77185a29
Jeremiah Kistler <jeremiahkistler@gmail.com>
2015-12-28 20:50:42 -0600

    Add custom navigation controller class.

---
+++ deepThoughts/Controllers/NavigationController.h
@@ -0,0 +1,14 @@
+//
+//  NavigationControllerDelegate.h
+//  deepThoughts
+//
+//  Created by Jeremiah Kistler on 12/28/15.
+//  Copyright © 2015 Jeremiah Kistler. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface NavigationController : NSObject <UINavigationControllerDelegate>
+
+@end
---
+++ deepThoughts/Controllers/NavigationController.m
@@ -0,0 +1,13 @@
+//
+//  NavigationControllerDelegate.m
+//  deepThoughts
+//
+//  Created by Jeremiah Kistler on 12/28/15.
+//  Copyright © 2015 Jeremiah Kistler. All rights reserved.
+//
+
+#import "NavigationController.h"
+
+@implementation NavigationController
+
+@end
