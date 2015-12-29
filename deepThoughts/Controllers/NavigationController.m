98665b5d68fa9cb3f8528d2f18f7ed99b0804914
Jeremiah Kistler <jeremiahkistler@gmail.com>
2015-12-29 08:18:07 -0600

    Implemented circle animation in the navigation controller.

--- deepThoughts/Controllers/NavigationController.m
+++ deepThoughts/Controllers/NavigationController.m
@@ -7,7 +7,17 @@
 //
 
 #import "NavigationController.h"
+#import "CircleTransitionAnimator.h"
+
+@interface NavigationController ()
+
+@end
 
 @implementation NavigationController
+
+-(id<UIViewControllerAnimatedTransitioning>)navigationController:(UINavigationController *)navigationController animationControllerForOperation:(UINavigationControllerOperation)operation fromViewController:(UIViewController *)fromVC toViewController:(UIViewController *)toVC {
+    
+    return [[CircleTransitionAnimator alloc] init];
+}
 
 @end
