46a60e4e65b8a4a0b085d6d303a04bbcd04224b5
Jeremiah Kistler <jeremiahkistler@gmail.com>
2016-01-01 22:01:15 -0600

    Add tableViewManager for settingsView.

---
+++ deepThoughts/SettingsViewTableManager.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewTableManager.h
+//  deepThoughts
+//
+//  Created by Jeremiah Kistler on 1/1/16.
+//  Copyright © 2016 Jeremiah Kistler. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SettingsViewTableManager : NSObject
+
+@end
---
+++ deepThoughts/SettingsViewTableManager.m
@@ -0,0 +1,13 @@
+//
+//  SettingsViewTableManager.m
+//  deepThoughts
+//
+//  Created by Jeremiah Kistler on 1/1/16.
+//  Copyright © 2016 Jeremiah Kistler. All rights reserved.
+//
+
+#import "SettingsViewTableManager.h"
+
+@implementation SettingsViewTableManager
+
+@end
