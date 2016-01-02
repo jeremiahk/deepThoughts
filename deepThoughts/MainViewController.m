e7d5c6a63760554eeedac8444f87219e19291d86
Jeremiah Kistler <jeremiahkistler@gmail.com>
2016-01-01 21:24:49 -0600

    Add animation to mainViewController.

--- deepThoughts/MainViewController.m
+++ deepThoughts/MainViewController.m
@@ -20,14 +20,35 @@
 
 - (void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
+    
+    CGAffineTransform transform = CGAffineTransformScale(CGAffineTransformIdentity, 0.0, 0.0);
+    _button.transform = transform;
 }
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    return [[UITableViewCell alloc] init];
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    [self animateButton];
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 1;
+- (void)animateButton
+{
+    [UIView animateWithDuration: 0.2 animations: ^{
+        CGAffineTransform transform = CGAffineTransformScale(CGAffineTransformIdentity, 1.25, 1.25);
+        _button.transform = transform;
+    } completion: ^(BOOL finished) {
+        
+        [UIView animateWithDuration:0.20 animations: ^{
+            CGAffineTransform transform = CGAffineTransformScale(CGAffineTransformIdentity, 0.95, 0.95);
+            _button.transform = transform;
+        } completion: ^(BOOL finished) {
+            
+            [UIView animateWithDuration:0.25 animations: ^{
+                CGAffineTransform transform = CGAffineTransformScale(CGAffineTransformIdentity, 1.0, 1.0);
+                _button.transform = transform;
+            }];
+        }];
+    }];
 }
 
 
