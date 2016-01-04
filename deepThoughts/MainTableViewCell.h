84e3266d2d036424c6991830f838a2af3210da8b
Jeremiah Kistler <jeremiahkistler@gmail.com>
2016-01-03 20:48:35 -0600

    Add tableViewCell for main screen.

---
+++ deepThoughts/MainTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MainTableViewCell.h
+//  deepThoughts
+//
+//  Created by Jeremiah Kistler on 1/3/16.
+//  Copyright © 2016 Jeremiah Kistler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainTableViewCell : UITableViewCell
+
+@end
---
+++ deepThoughts/MainTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  MainTableViewCell.m
+//  deepThoughts
+//
+//  Created by Jeremiah Kistler on 1/3/16.
+//  Copyright © 2016 Jeremiah Kistler. All rights reserved.
+//
+
+#import "MainTableViewCell.h"
+
+@implementation MainTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
